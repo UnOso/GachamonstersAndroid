22952d58d37eae03a0e1912848860482981adb5f
UnOso <manuelluis736@gmail.com>
2021-11-09 13:40:36 +0100

    Menu Gachamon

--- app/src/main/java/com/polariumx/gachamonsters/features/MainMenuFragment.kt
+++ app/src/main/java/com/polariumx/gachamonsters/features/MainMenuFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.polariumx.gachamonsters.R
@@ -43,6 +44,8 @@ class MainMenuFragment : Fragment(),
             setHasFixedSize(true)
             adapter = MainMenuOptionGachamonstersStatsAdapter(stats)
         }
+
+        binding.fragmentMainMenuGachamonstersImageView.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.mobu))
 
     }
 
