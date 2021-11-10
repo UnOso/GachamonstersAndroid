1a8934cd394fb584356e0082b2630f4aa67bcc90
UnOso <manuelluis736@gmail.com>
2021-11-10 14:15:18 +0100

    Starting Changes

--- app/src/main/java/com/polariumx/gachamonsters/features/mainmenu/model/MainMenuOptionModel.kt
+++ app/src/main/java/com/polariumx/gachamonsters/features/mainmenu/model/MainMenuOptionModel.kt
@@ -1,8 +1,9 @@
-package com.polariumx.gachamonsters.features.model
+package com.polariumx.gachamonsters.features.mainmenu.model
 
 import android.graphics.drawable.Drawable
 
 data class MainMenuOptionModel (
+    val id: Int,
     val optionText : String,
     val optionDrawable: Drawable?,
         )
