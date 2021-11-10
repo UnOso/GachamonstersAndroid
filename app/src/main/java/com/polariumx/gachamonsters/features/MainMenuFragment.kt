854166ee49ba027fddea877ac929bfbc65c0bbf5
Luis Manuel <85862638+UnOso@users.noreply.github.com>
2021-11-10 10:16:19 +0100

    Gachamon view main menu
    * MainMenuFragment, main menu slider, list item
    
    * OptionModel, Adapter
    
    * Options Slider Working
    
    * Ver_1 Option Slider
    
    * Initial Layout Gachamon Main Menu
    
    * Menu Config
    
    * Menu Config
    
    * Menu Gachamon
    
    * MainMenuFragment, main menu slider, list item
    
    * Options Slider Working
    
    * Main menu feature (#1)
    
    * MainMenuFragment, main menu slider, list item
    
    * OptionModel, Adapter
    
    * Options Slider Working
    
    * Ver_1 Option Slider

--- app/src/main/java/com/polariumx/gachamonsters/features/MainMenuFragment.kt
+++ app/src/main/java/com/polariumx/gachamonsters/features/MainMenuFragment.kt
@@ -5,17 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.polariumx.gachamonsters.R
 import com.polariumx.gachamonsters.databinding.FragmentMainMenuBinding
+import com.polariumx.gachamonsters.features.model.MainMenuGachaStatModel
 import com.polariumx.gachamonsters.features.model.MainMenuOptionModel
 
 class MainMenuFragment : Fragment(),
-    MainMenuOptionSliderRecyclerAdapter.OptionItemListener{
+    MainMenuOptionSliderRecyclerAdapter.OptionItemListener {
 
     private lateinit var binding: FragmentMainMenuBinding
     private lateinit var options: Array<MainMenuOptionModel>
+    private lateinit var stats: Array<MainMenuGachaStatModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +26,7 @@ class MainMenuFragment : Fragment(),
     ): View {
         binding = FragmentMainMenuBinding.inflate(inflater, container, false)
         return binding.root
-    }
+    }  
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,20 +34,40 @@ class MainMenuFragment : Fragment(),
         setupUI()
     }
 
-    private fun setupUI(){
+    private fun setupUI() {
         binding.fragmentMainMenuOptionSliderRecyclerView.apply {
             setHasFixedSize(true)
             adapter = MainMenuOptionSliderRecyclerAdapter(options, this@MainMenuFragment)
         }
+
+        binding.fragmentMainMenuGachamonstersStatsRecyclerView.apply {
+            setHasFixedSize(true)
+            adapter = MainMenuOptionGachamonstersStatsAdapter(stats)
+        }
+
+        binding.fragmentMainMenuGachamonstersImageView.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.mobu))
+
     }
 
-    private fun setupConfig(){
+    private fun setupConfig() {
         options = arrayOf(
             MainMenuOptionModel("Play", ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_play_circle)),
             MainMenuOptionModel("Team", ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_team_option)),
-            MainMenuOptionModel("Inventory", ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_inventory_option)),
+            MainMenuOptionModel(
+                "Inventory",
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_inventory_option)
+            ),
             MainMenuOptionModel("Options", ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_settings_option)),
             MainMenuOptionModel("Exit", ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_exit_to_app_option))
+        )
+
+        stats = arrayOf(
+            MainMenuGachaStatModel("HP", 10),
+            MainMenuGachaStatModel("ATK", 1),
+            MainMenuGachaStatModel("DEF", 1),
+            MainMenuGachaStatModel("M.ATK", 1),
+            MainMenuGachaStatModel("M.DEF", 1),
+            MainMenuGachaStatModel("SPEED", 1)
         )
     }
 
