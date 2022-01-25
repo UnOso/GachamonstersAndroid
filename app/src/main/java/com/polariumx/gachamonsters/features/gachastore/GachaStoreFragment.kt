9ffbc8e08a40e518ab1630d1fb62d70376bb1e40
Oso <manuelluis736@gmail.com>
2022-01-25 12:31:42 +0100

    Gachamon Store XML and Logic

--- app/src/main/java/com/polariumx/gachamonsters/features/gachastore/GachaStoreFragment.kt
+++ app/src/main/java/com/polariumx/gachamonsters/features/gachastore/GachaStoreFragment.kt
@@ -1,10 +1,14 @@
 package com.polariumx.gachamonsters.features.gachastore
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.polariumx.gachamonsters.R
 import com.polariumx.gachamonsters.databinding.FragmentGachaStoreBinding
 
@@ -21,10 +25,24 @@ class GachaStoreFragment : Fragment() {
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
     }
 
+    private fun setupUI(){
+        binding.fragmentGachamonStoreExitButton.setOnClickListener(View.OnClickListener {
+            findNavController().navigateUp()
+        })
+        binding.fragmentGachamonStorePermaImage.setOnClickListener {
+            changeSrc(ContextCompat.getColor(requireContext(),R.color.red))
+        }
+        binding.fragmentGachamonStorePromoImage.setOnClickListener {
+            changeSrc(ContextCompat.getColor(requireContext(),R.color.blue))
+        }
+    }
+
+    private fun changeSrc(tintColor : Int){
+        binding.fragmentGachamonStoreChosenBannerImage.setColorFilter(tintColor);
+    }
 }
